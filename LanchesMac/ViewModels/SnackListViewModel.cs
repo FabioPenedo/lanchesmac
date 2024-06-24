5da7f2280ec4d83b254d806454061ef7271020fc
FabioPenedo <fabiopenedo21@gmail.com>
2024-06-24 17:48:57 -0300

    criando a partial view _SnacksSummary

---
+++ LanchesMac/ViewModels/SnackListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.ViewModels
+{
+    public class SnackListViewModel
+    {
+        public IEnumerable<Snack> Snacks { get; set; } = Enumerable.Empty<Snack>();
+        public string CurrentCategory { get; set; } = string.Empty;
+    }
+}
