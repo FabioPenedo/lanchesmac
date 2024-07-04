f65da696a907a524243c651b41866c5256b113f4
FabioPenedo <fabiopenedo21@gmail.com>
2024-07-04 14:55:38 -0300

    criado view component para exibir categorias no menu lanches

---
+++ LanchesMac/Components/MenuCategory.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Components
+{
+    public class MenuCategory : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public MenuCategory(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.Categories.OrderBy(c => c.Name);
+            return View(categories);
+        }
+    }
+}
