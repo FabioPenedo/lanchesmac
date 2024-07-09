2684459a61df0f0928e5fce75688bd682aaf588b
FabioPenedo <fabiopenedo21@gmail.com>
2024-07-09 16:25:31 -0300

    criando o controlador OrderController

---
+++ LanchesMac/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly CartPurchase _cartPurchase;
+
+        public OrderController(IOrderRepository orderRepository, CartPurchase cartPurchase)
+        {
+            _orderRepository = orderRepository;
+            _cartPurchase = cartPurchase;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            return View();
+        }
+    }
+}
