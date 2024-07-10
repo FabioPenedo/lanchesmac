33fd9098d2be6621ce1c6bf59fe39f933c088980
FabioPenedo <fabiopenedo21@gmail.com>
2024-07-10 16:20:34 -0300

    concluido o metodo checkout post e criando a view CheckoutCompleto

--- LanchesMac/Controllers/OrderController.cs
+++ LanchesMac/Controllers/OrderController.cs
@@ -24,7 +24,47 @@ namespace LanchesMac.Controllers
         [HttpPost]
         public IActionResult Checkout(Order order)
         {
-            return View();
+            int totalItemsOrdered = 0;
+            decimal priceTotalOrdered = 0.0m;
+
+            //obtem os itens do carrinho de compra do cliente
+            List<CartPurchaseItem> items = _cartPurchase.GetCartPurchaseItens();
+            _cartPurchase.Items = items;
+
+            //verifica se existem itens de pedido
+            if(_cartPurchase.Items.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche...");
+            }
+
+            //calcula o total de itens e o total de pedido
+            foreach (var item in items)
+            {
+                totalItemsOrdered += item.Amount;
+                priceTotalOrdered += (item.Snack.Price * item.Amount);
+            }
+
+            //atribui os valores obtidos ao pedido
+            order.TotalItemsOrdered = totalItemsOrdered;
+            order.TotalOrder = priceTotalOrdered;
+
+            //valida os dados do pedido
+            if(ModelState.IsValid)
+            {
+                //cria o pedido e os detalhes
+                _orderRepository.CreateOrder(order);
+
+                //define mensagens ao cliente
+                ViewBag.CheckoutCompleteMessage = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalOrder = _cartPurchase.GetCartTotalPurchase();
+
+                //limpa o carrinho do cliente
+                _cartPurchase.CleanCart();
+
+                //exibe a view com dados do cliente e do pedido
+                return View("~/Views/Order/CheckoutComplete.cshtml", order);
+            }
+            return View(order);
         }
     }
 }
